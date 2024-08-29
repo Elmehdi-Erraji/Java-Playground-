8fd763b3773e2ce402fcd7406a8d255d3d9357e1
Mehdi <elmehdiworkemail@gmail.com>
2024-08-29 11:01:28 +0100

    learning the java basic syntax

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your name please : ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter age: ");
+        int age = scanner.nextInt();
+
+        System.out.println("Your name is " + name + " and name is " + age);
+
+        scanner.close();
+
+    }
+}
