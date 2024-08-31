991ed6eea3392dbfe36b1dbe48572a835684c1fc
Mehdi <elmehdiworkemail@gmail.com>
2024-08-31 19:20:41 +0100

    working on the setters and getters

--- src/OOP/Person.java
+++ src/OOP/Person.java
@@ -8,5 +8,11 @@ public class Person {
         this.name = name;
         this.age = age;
     }
-
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
