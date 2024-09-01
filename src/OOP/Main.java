d18fac120b4c6c9643df2d6a44c546748c891cb6
Mehdi <elmehdiworkemail@gmail.com>
2024-09-01 20:48:31 +0100

    testing stuff

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -5,7 +5,11 @@ public class Main {
 
         System.out.println("Welcome to the Carbon Consumption Management System!");
         System.out.println("Welcome to the Carbon Consumption Management System!");
-        
+        System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");
+
+
 
 
     }
