34646846d2c171e19f893b7b6409f53320902def
Mehdi <elmehdiworkemail@gmail.com>
2024-09-01 20:47:57 +0100

    testing stuff

--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -2,7 +2,10 @@ package src.OOP;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");
+        
 
 
     }
