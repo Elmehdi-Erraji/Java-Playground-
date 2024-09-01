8dc599ea166f8c3fc60c109928b11408bd3109c4
Mehdi <elmehdiworkemail@gmail.com>
2024-09-01 20:50:31 +0100

    working on ds

--- src/DS/Lists.java
+++ src/DS/Lists.java
@@ -2,7 +2,4 @@ package src.DS;
 
 public class Lists {
 
-  System.out.println("Welcome to the Carbon Consumption Management System!");
-        System.out.println("Welcome to the Carbon Consumption Management System!");  System.out.println("Welcome to the Carbon Consumption Management System!");
-        System.out.println("Welcome to the Carbon Consumption Management System!");
 }
