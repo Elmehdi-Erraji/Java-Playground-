ec95eecfe1aff02b8bd9f97bbfb3f9ba39ccbd71
Mehdi <elmehdiworkemail@gmail.com>
2024-09-01 20:50:16 +0100

    working on ds

--- src/DS/Lists.java
+++ src/DS/Lists.java
@@ -2,5 +2,7 @@ package src.DS;
 
 public class Lists {
 
-
+  System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");  System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");
 }
--- src/OOP/Main.java
+++ src/OOP/Main.java
@@ -5,7 +5,10 @@ public class Main {
 
         System.out.println("Welcome to the Carbon Consumption Management System!");
         System.out.println("Welcome to the Carbon Consumption Management System!");
-      
+        System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");  System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");  System.out.println("Welcome to the Carbon Consumption Management System!");
+        System.out.println("Welcome to the Carbon Consumption Management System!");
 
 
 
