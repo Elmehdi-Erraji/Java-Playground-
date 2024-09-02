e9e3a6210058254d90b1d1a52708b967c16be393
Mehdi <elmehdiworkemail@gmail.com>
2024-09-02 14:59:07 +0100

    the third problem was really hard

---
+++ src/DS/study/SubArrays.java
@@ -0,0 +1,8 @@
+package src.DS.study;
+
+public class SubArrays {
+    public static void main(String[] args) {
+        int [] arr1 = {0, 1, 0, 3, 12};
+        int [][] arr2 = new int[(arr1.length )][arr1.length];
+    }
+}
